a65556f7480b2d71267347ec2f0e802743340204
Tim Smith <timothys740@gmail.com>
2017-05-20 17:11:47 -0400

    POS finder and scorer

--- src/processors.java
+++ src/processors.java
@@ -1,9 +1,9 @@
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
-import edu.stanford.nlp.io.IOUtils;
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
@@ -11,8 +11,6 @@ import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-import edu.stanford.nlp.semgraph.SemanticGraph;
-import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations.CollapsedCCProcessedDependenciesAnnotation;
 import edu.stanford.nlp.util.CoreMap;
 
 public class processors {
@@ -47,23 +45,163 @@ public class processors {
 	    }
 		
 		Properties props = new Properties();
-	    props.setProperty("annotators", "tokenize, ssplit, pos, parse");
+	    props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
 
 	    StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
 
 	    // Initialize an Annotation with some text to be annotated. The text is the argument to the constructor.
 	    Annotation annotation;
-	    if (args.length > 0) {
-	      annotation = new Annotation(IOUtils.slurpFileNoExceptions(args[0]));
-	    } else {
-	      annotation = new Annotation("Who was president during the Civil War.");
-	    }
+	    annotation = new Annotation("Was Lincoln President? Abraham Lincoln was president.");
+	    
 
 	    // run all the selected Annotators on this text
 	    pipeline.annotate(annotation);
+	    int test = 0;
+	    ArrayList<String> questNounsProper = new ArrayList<String>();
+	    ArrayList<String> questNouns = new ArrayList<String>();
+	    ArrayList<String> questVerbs = new ArrayList<String>();
+	    ArrayList<String> ansNounsProper = new ArrayList<String>();
+	    ArrayList<String> ansNouns = new ArrayList<String>();
+	    ArrayList<String> ansVerbs = new ArrayList<String>();
+	    String question;
+	    String answer;
 	    
 	    List<CoreMap> sentences = annotation.get(CoreAnnotations.SentencesAnnotation.class);
 	    for (CoreMap sentence: sentences){
+	    	if (test == 0){
+	    		question = sentence.toString();
+	    	}
+	    	if (test == 1){
+	    		answer = sentence.toString();
+	    	}
+	    	for (CoreLabel token: sentence.get(TokensAnnotation.class)){
+	    		String pos = token.get(PartOfSpeechAnnotation.class);
+	    		String word = token.get(TextAnnotation.class);
+	    		System.out.println(pos);
+	    		if (test == 0){
+	    			if (pos.equals("NNP")){
+	    				questNounsProper.add(word);
+	    			} else if (pos.equals("NN")){
+	    				questNouns.add(word);
+	    			} else if (pos.equals("VBD")){
+	    				System.out.println("Test");
+	    				questVerbs.add(word);
+	    			} else if (pos.equals("VB")){
+	    				questVerbs.add(word);
+	    			}
+	    		}
+	    		if (test == 1){
+	    			if (pos.equals("NNP")){
+	    				ansNounsProper.add(word);
+	    			} else if (pos.equals("NN")){
+	    				ansNouns.add(word);
+	    			} else if (pos.equals("VBD")){
+	    				System.out.println("Test");
+	    				ansVerbs.add(word);
+	    			} else if (pos.equals("VB")){
+	    				ansVerbs.add(word);
+	    			}
+	    		}
+	    	}
+	    	test++;
+	    }
+	    int score = 0;
+	    for (String current : questNounsProper){
+	    	System.out.println(current);
+	    	if (ansNounsProper.contains(current)){
+	    		score++;
+	    	}
+	    }
+	    int nounsProper = questNounsProper.size() / score;
+	    score = 0;
+	    for (String current : questNouns){
+	    	if (ansNounsProper.contains(current)){
+	    		score++;
+	    	}
+	    }
+	    int nouns = 0;
+	    if (score != 0){
+	    	nouns = questNouns.size() / score;
+	    }
+	    score = 0;
+	    for (String current : questVerbs){
+	    	if (ansVerbs.contains(current)){
+	    		score++;
+	    	}
+	    }
+	    System.out.println(score);
+	    int verbs = 0;
+	    if (score != 0){
+	    	verbs = questVerbs.size() / score;
+	    }
+	    System.out.println(nounsProper);
+	    System.out.println(nouns);
+	    System.out.println(verbs);
+	    	/*SemanticGraph dependencies = sentence.get(CollapsedCCProcessedDependenciesAnnotation.class);
+    		System.out.println(dependencies.toList());
+    		System.out.println(dependencies.toPOSList());
+    		System.out.println(dependencies.toString());*/
+	    	/*Tree tree = sentence.get(TreeCoreAnnotations.TreeAnnotation.class);
+		    out.println("The first sentence parse tree is:");
+		    tree.pennPrint(out);
+		    System.out.println(tree.getChildrenAsList());
+		    
+		    Tree[] children = tree.children();
+		    System.out.println(tree.pennString().split(" ").toString());
+		    for (Tree child: children){
+		    	System.out.println("test");
+		    	System.out.println(child.flatten());
+		    	//if (child.value().equals("S")){
+		    		List<Tree> leaves = child.getLeaves();
+		    		for (Tree leaf : leaves){
+		    			leaf.headTerminal(hf, parent)
+		    			//if(leaf.value().equals("NP")){
+		    			List<Word> words = leaf.yieldWords();
+		    			for (Word word: words){
+		    				System.out.println(String.format("(%s - NP),",word.word()));
+		    			}
+		    			//}
+		    		}
+		    	//}
+		    }*/
+	    
+	    
+	    /*Map<Integer, CorefChain> graph = annotation.get(CorefChainAnnotation.class);
+	    System.out.println(graph.keySet());*/
+		
+		/*Annotation document = new Annotation("Barack Obama was born in Hawaii.  He is the president. Obama was elected in 2008.");
+	    Properties props = new Properties();
+	    props.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner,parse,mention,coref");
+	    StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+	    pipeline.annotate(document);
+	    System.out.println("---");
+	    System.out.println("coref chains");
+	    for (edu.stanford.nlp.coref.data.CorefChain cc : document.get(CorefCoreAnnotations.CorefChainAnnotation.class).values()) {
+	      System.out.println("\t" + cc);
+	    }
+	    for (CoreMap sentence : document.get(CoreAnnotations.SentencesAnnotation.class)) {
+	      System.out.println("---");
+	      System.out.println("mentions");
+	      for (Mention m : sentence.get(CorefCoreAnnotations.CorefMentionsAnnotation.class)) {
+	        System.out.println("\t" + m);
+	       }
+	    }*/
+		
+		/*Properties props = new Properties();
+	    props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
+
+	    StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+
+	    // Initialize an Annotation with some text to be annotated. The text is the argument to the constructor.
+	    Annotation annotation;
+	    
+	    annotation = new Annotation("Who was president during the Civil War.");
+
+
+	    // run all the selected Annotators on this text
+	    pipeline.annotate(annotation);
+		List<CoreMap> sentences = annotation.get(CoreAnnotations.SentencesAnnotation.class);*/
+	    /*for (CoreMap sentence: sentences){
 	    	for (CoreLabel token: sentence.get(TokensAnnotation.class)){
 	    		String pos = token.get(PartOfSpeechAnnotation.class);
 	    		String word = token.get(TextAnnotation.class);
@@ -73,20 +211,29 @@ public class processors {
 	    	}
 	    	SemanticGraph dependencies = sentence.get(CollapsedCCProcessedDependenciesAnnotation.class);
     		System.out.println(dependencies.toList());
-	    }
-	    
-	    /*CoreMap sentence = sentences.get(0);
-	    
-	    Tree tree = sentence.get(TreeCoreAnnotations.TreeAnnotation.class);
-	    out.println("The first sentence parse tree is:");
-	    tree.pennPrint(out);
-	    System.out.println(tree.getChildrenAsList());
-	    System.out.println(tree.toString().split("))").length);
-	    
-	    out.println();*/
-	    
-	    
+    		System.out.println(dependencies.toPOSList());
+    		System.out.println(dependencies.toString());
+	    }*/
+		/*CoreMap sentence = sentences.get(0);
+		Tree parse = sentence.get(TreeCoreAnnotations.TreeAnnotation.class);
+		List<String> result = new ArrayList<>();
+	    TregexPattern pattern = TregexPattern.compile("@NP");
+	    TregexMatcher matcher = pattern.matcher(parse);
+	    while (matcher.find()) {
+	        Tree match = matcher.getMatch();
+	        List<Tree> leaves = match.getLeaves();
+	        System.out.println(leaves);
+	        // Some Guava magic.
+	        String nounPhrase = Joiner.on(' ').join(Lists.transform(leaves, Functions.toStringFunction()));
+	        result.add(nounPhrase);
+	        List<LabeledWord> labeledYield = match.labeledYield();
+	        System.out.println("labeledYield: " + labeledYield);
+	    }*/
 	    
 	}
+	
+	/*public static void dfs(Tree node, Tree parent, HeadFinder headFinder){
+		
+	}*/
 
 }
